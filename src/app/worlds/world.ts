23a384e1e43af66731c6e5f403faa63557c55c66
Ascyt <account@ascyt.com>
2024-06-24 12:15:35 +0200

    Added material and pickaxe interfaces

--- src/app/worlds/world.ts
+++ src/app/worlds/world.ts
@@ -1,2 +1,13 @@
+import { Pickaxe } from "./pickaxe";
+import { Material } from "./material";
+
 export interface World {
+    name:string,
+
+    materials:Material[],
+    pickaxes:Pickaxe[],
+
+    corruptionLevel:number,
+
+    connectedTo:World[],
 }
--- src/app/worlds/worlds.service.ts
+++ src/app/worlds/worlds.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { World } from './world';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorldsService {
+  public currentWorld:World;
 
-  constructor() { }
+  constructor() { 
+    this.currentWorld = {
+      name: 'The World',
+      materials: [],
+      pickaxes: [],
+      corruptionLevel: 0,
+      connectedTo: []
+    };
+  }
 }
