c566b5dac587343b491610a4c742d672826de346
Ascyt <account@ascyt.com>
2024-06-26 08:24:32 +0200

    Added initial default world values

--- src/app/worlds/pickaxe.ts
+++ src/app/worlds/pickaxe.ts
@@ -1,2 +1,18 @@
+import { Material } from "./material";
+
 export interface Pickaxe {
+    id:number;
+    name:string|null;
+    probabilities:MineProbability[];
+    requiredToCraft:CraftRequirement[]|null;
+}
+
+export interface MineProbability {
+    material:number;
+    probability:number;
+}
+
+export interface CraftRequirement {
+    material:number;
+    quantity:number;
 }
--- src/app/worlds/worlds.service.ts
+++ src/app/worlds/worlds.service.ts
@@ -10,8 +10,130 @@ export class WorldsService {
   constructor() { 
     this.currentWorld = {
       name: 'The World',
-      materials: [],
-      pickaxes: [],
+      materials: [
+        { 
+          id: 1,
+          name: 'stone'
+        },
+        { 
+          id: 2,
+          name: 'iron'
+        },
+        {
+          id: 3,
+          name: 'gold'
+        },
+        { 
+          id: 4,
+          name: 'diamond'
+        }
+      ],
+
+      pickaxes: [
+        {
+          id: 1,
+          name: null,
+          probabilities: [
+            { 
+              material: 1,
+              probability: 1
+            }
+          ],
+          requiredToCraft: null
+        },
+        {
+          id: 2,
+          name: 'stone pickaxe',
+          probabilities: [
+            { 
+              material: 1,
+              probability: 1
+            },
+            { 
+              material: 2,
+              probability: 0.5
+            }
+          ],
+          requiredToCraft: [{ 
+            material: 1,
+            quantity: 3
+          }]
+        },        
+        {
+          id: 3,
+          name: 'iron pickaxe',
+          probabilities: [
+            { 
+              material: 1,
+              probability: 1
+            },
+            { 
+              material: 2,
+              probability: 0.5
+            },
+            { 
+              material: 3,
+              probability: 0.25
+            }
+          ],
+          requiredToCraft: [{ 
+            material: 2,
+            quantity: 3
+          }]
+        },
+        {
+          id: 4,
+          name: 'gold pickaxe',
+          probabilities: [
+            { 
+              material: 1,
+              probability: 1
+            },
+            { 
+              material: 2,
+              probability: 0.5
+            },
+            { 
+              material: 3,
+              probability: 0.25
+            },
+            { 
+              material: 4,
+              probability: 0.125
+            }
+          ],
+          requiredToCraft: [{ 
+            material: 3,
+            quantity: 3
+          }]
+        },
+        {
+          id: 5,
+          name: 'diamond pickaxe',
+          probabilities: [
+            { 
+              material: 1,
+              probability: 1
+            },
+            { 
+              material: 2,
+              probability: 1
+            },
+            { 
+              material: 3,
+              probability: 0.5
+            },
+            { 
+              material: 4,
+              probability: 0.25
+            }
+          ],
+          requiredToCraft: [{ 
+            material: 4,
+            quantity: 3
+          }]
+        }
+      ],
       corruptionLevel: 0,
       connectedTo: []
     };
